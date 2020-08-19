95c9904c9cd6e2853c1da68b5c86e56ef54798e0
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2020-08-19 01:08:48 -0600

    creacion del proyecto funciones

---
+++ src/clase1/clase1.java
@@ -0,0 +1,22 @@
+
+package clase1;
+
+public class clase1 {
+
+   
+    public static void main(String[] args) {  
+//  funcionesMatematicas funciones = new funcionesMatematicas();
+//        System.out.println(funciones.division());
+//      
+    
+    coche miCarro = new coche ("TOYOTA",2019,"ROJO");
+    coche tuCarro = new coche ("Nissan",2019,"Negro");
+    
+        System.out.println(tuCarro.toString());
+    
+    
+  }
+    
+}
+     
+ 
---
+++ src/clase1/funcionesMatematicas.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1;
+
+/**
+ *
+ * @author carlj
+ */
+public class funcionesMatematicas {
+   
+
+//    System.out.println("El resultado de la operacion "+resultado);
+    
+    int suma (){
+    
+        int dato1= 21;
+    int dato2= 10;
+    int resultado;
+    
+    resultado = dato1+ dato2;
+    
+    return resultado;
+    
+    }
+    
+    
+    int resta (){
+        
+    int dato1= 21;
+    int dato2= 10;
+    int resultado;
+    
+    resultado = dato1- dato2;
+    
+    return resultado;
+    
+    }
+    
+    double  division(){
+      int dato1= 21;
+    int dato2= 10;
+    int resultado;
+    
+    resultado = dato1/ dato2;
+    return resultado;
+    }
+    
+    }
+            
+    
+
